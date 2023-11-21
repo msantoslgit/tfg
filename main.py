fbe4a0fc2379fa52997bdbcdf2ef9737e008af26
Mario Santos <msantosl@eurofirms.es>
2023-11-21 16:03:13 +0100

    First API connection / test1

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ client = OpenAI(api_key = API_KEY)
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    {"role": "system", "content": "You are an expert translator."},
+    {"role": "user", "content": "Translate te following word to spanish and catalan: Teddy Bear."}
   ]
 )
 
