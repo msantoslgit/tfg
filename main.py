80dd4c5c62fd922c9e73f22237a93eb879ee5c02
Mario Santos <msantosl@eurofirms.es>
2023-12-04 13:42:08 +0100

    First Iteration / Conversation Working only with questions

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from openai import OpenAI
 
 #Files
 from component import API_KEY
-from openAI_functions import get_response, close_session
+from openAI_functions import get_response, close_session, add_context_response
 
 
 def main():
 
     client = OpenAI(api_key=API_KEY)
     model = "gpt-3.5-turbo"
+    max_tokens = 50
 
     # Inicia la conversación con un mensaje de sistema
     conversation = [{"role": "system",
@@ -20,11 +21,13 @@ def main():
         content = input("¿Sobre que quieres hablar? ")
 
         if content == "exit":
-            close_session(client, model, conversation)
+            close_session(client, model, conversation, max_tokens)
             break
 
-        response = get_response(client, model, conversation, content)
+        response = get_response(client, model, conversation, content, max_tokens)
+
         print(response)
+        add_context_response(conversation, response)
 
 
 if __name__ == "__main__":
--- openAI_functions.py
+++ openAI_functions.py
@@ -1,29 +1,35 @@
 from openai import OpenAI
 
 
-def get_response(client, model, conversation, content):
+def get_response(client, model, conversation, content, max_tokens):
     conversation.append({"role": "user", "content": content})
 
     completion = client.chat.completions.create(
         model=model,
-        messages=conversation
+        messages=conversation,
+        max_tokens=max_tokens
     )
     # assistant_response = completion.choices[0].message['content']
-    assistant_response = completion.choices[0].message
+    assistant_response = completion.choices[0].message.content
 
     return assistant_response
 
 
-def close_session(client, model, conversation):
+def add_context_response(conversation, response):
+    conversation.append({"role": "assistant", "content": response})
+
+
+def close_session(client, model, conversation, max_tokens):
     # Agrega un mensaje para cerrar la sesión
     conversation.append({"role": "system", "content": "Session Ended."})
 
     # Realiza una última llamada para cerrar la sesión
     completion = client.chat.completions.create(
         model=model,
-        messages=conversation
+        messages=conversation,
+        max_tokens=max_tokens
     )
     # assistant_response = completion.choices[0].message['content']
-    assistant_response = completion.choices[0].message
+    assistant_response = completion.choices[0].message.content
     print(assistant_response)
 
