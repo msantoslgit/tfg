db9f0aed11d2df79335a06aa45fb3b2996bedfd4
Mario Santos <msantosl@eurofirms.es>
2023-12-05 13:40:00 +0100

    first version reading a database from a source folder

---
+++ database_functions.py
@@ -0,0 +1,82 @@
+import os
+
+
+# def get_db_directory():
+#     # Obtén la ruta del directorio actual del script
+#     script_directory = os.path.dirname(os.path.abspath(__file__))
+#
+#     # Construye la ruta al directorio un nivel por encima
+#     parent_directory = os.path.abspath(os.path.join(script_directory, ".."))
+#
+#     # Construye la ruta completa al directorio que necesitas
+#     target_directory = os.path.join(parent_directory,"tfg", "source", "DB", "DUMMY_1")
+#
+#     # Ahora puedes utilizar target_directory en tu código
+#     return target_directory
+
+
+def get_available_db_directories():
+    # Obtén la ruta del directorio actual del script
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Construye la ruta al directorio un nivel por encima
+    parent_directory = os.path.abspath(os.path.join(script_directory, ".."))
+
+    # Construye la ruta al directorio que contiene las carpetas a elegir
+    choices_directory = os.path.join(parent_directory, "tfg", "source", "DB")
+
+    # Lista los directorios disponibles en el nivel deseado
+    available_directories = [d for d in os.listdir(choices_directory) if os.path.isdir(os.path.join(choices_directory, d))]
+
+    # Muestra los directorios disponibles por pantalla
+    print("Directorios disponibles:")
+    for index, directory in enumerate(available_directories, start=1):
+        print(f"{index}. {directory}" + '\n')
+
+    # Pide al usuario que elija un directorio
+    while True:
+        try:
+            selected_index = int(input("Selecciona un número correspondiente al directorio deseado: ")) - 1
+
+            # Verifica si el número ingresado es válido
+            if 0 <= selected_index < len(available_directories):
+                # Si el número es válido, salimos del bucle
+                break
+            else:
+                print("Por favor, ingresa un número dentro del rango." + '\n')
+                print("Directorios disponibles:")
+                for index, directory in enumerate(available_directories, start=1):
+                    print(f"{index}. {directory}" + '\n')
+        except ValueError:
+            print("Por favor, ingresa un número entero." + '\n')
+            print("Directorios disponibles:")
+            for index, directory in enumerate(available_directories, start=1):
+                print(f"{index}. {directory}" + '\n')
+
+    # Construye la ruta completa al directorio seleccionado
+    selected_directory = os.path.join(choices_directory, available_directories[selected_index])
+
+    # Retorna la ruta del directorio seleccionado
+    return selected_directory
+
+
+def read_db_txt(directory_path):
+    # Check if the path is valid
+    if not os.path.isdir(directory_path):
+        return "The provided path is not a valid directory."
+
+    # Initialize the string to concatenate the content
+    concatenated_content = ""
+
+    # Iterate through all files in the directory
+    for file_name in os.listdir(directory_path):
+        # Check if the file has a .txt extension
+        if file_name.endswith(".txt"):
+            # Build the full file path
+            file_path = os.path.join(directory_path, file_name)
+
+            # Read the content of the file and concatenate it to the string
+            with open(file_path, 'r') as file:
+                concatenated_content += file.read() + '\n' + '\n'
+
+    return concatenated_content
--- main.py
+++ main.py
@@ -4,31 +4,43 @@ from openai import OpenAI
 #Files
 from component import API_KEY
 from openAI_functions import get_response, close_session, add_context_response
-
+from database_functions import read_db_txt, get_available_db_directories #, get_db_directory
 
 def main():
 
-    client = OpenAI(api_key=API_KEY)
-    model = "gpt-3.5-turbo"
-    max_tokens = 50
+    # Example of usage get_db_directory
+    # example_path = get_db_directory()
+    # total_content = read_db_txt(example_path)
 
-    # Inicia la conversación con un mensaje de sistema
-    conversation = [{"role": "system",
-                     "content": "You are a helpful assistant."}]
+    # Example of usage get_available_db_directories
+    selected_db_directory = get_available_db_directories()
+    print(f"Directorio seleccionado: {selected_db_directory}")
 
-    print("¿Sobre que quieres hablar? ")
-    while True:
+    # Example of usage read_db_txt
+    total_content = read_db_txt(selected_db_directory)
+    print(total_content)
 
-        content = input(" ")
-
-        if content == "exit":
-            close_session(client, model, conversation, max_tokens)
-            break
-
-        response = get_response(client, model, conversation, content, max_tokens)
-
-        print(response)
-        add_context_response(conversation, response)
+    # client = OpenAI(api_key=API_KEY)
+    # model = "gpt-3.5-turbo"
+    # max_tokens = 50
+    #
+    # # Inicia la conversación con un mensaje de sistema
+    # conversation = [{"role": "system",
+    #                  "content": "You are a helpful assistant."}]
+    #
+    # print("¿Sobre que quieres hablar? ")
+    # while True:
+    #
+    #     content = input(" ")
+    #
+    #     if content == "exit":
+    #         close_session(client, model, conversation, max_tokens)
+    #         break
+    #
+    #     response = get_response(client, model, conversation, content, max_tokens)
+    #
+    #     print(response)
+    #     add_context_response(conversation, response)
 
 
 if __name__ == "__main__":
