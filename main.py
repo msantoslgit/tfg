383d4658e41a4b740344632232592892f807c842
Mario Santos <msantosl@eurofirms.es>
2024-01-04 11:33:19 +0100

    Added comments main.py

--- main.py
+++ main.py
@@ -10,23 +10,21 @@ from database_functions import get_available_db_directories, read_db_txt
 
 def main():
 
-    # Declaración de variables globales
+    # Declaration of global variables
     client = OpenAI(api_key=API_KEY)
     model = "gpt-3.5-turbo"
     max_tokens = 100
     price_per_token = 0.002 / 1000
 
-    print(init_prompt)
-
-    # Example of usage get_available_db_directories
+    # Usage of get_available_db_directories
     selected_db_directory = get_available_db_directories()
-    print(f"Directorio seleccionado: {selected_db_directory}")
+    print(f"Selected directory: {selected_db_directory}")
 
-    # Example of usage read_db_txt
+    # Usage of read_db_txt
     db_content = read_db_txt(selected_db_directory)
     print(db_content)
 
-    # Example of usage TokenPricing
+    # Example of using TokenPricing
     token_pricing = TokenPricing(max_tokens, price_per_token, model)
 
     num_tokens = token_pricing.num_tokens_from_string(init_prompt)
@@ -35,23 +33,18 @@ def main():
     print(f"Number of tokens: {num_tokens}")
     print(f"Total price: ${total_price}")
 
-    # Ejemplo de uso de la funcion OpenAIChat
+    # Start the conversation with a system message
+    init_conversation = [{"role": "system", "content": init_prompt}]
 
-    # Inicia la conversación con un mensaje de sistema
-    init_conversation = [{"role": "system",
-                          "content": init_prompt}]
+    # Initialize the OpenAIChat class with the model, max_tokens, API_KEY, and the initial conversation
+    openai_chat = OpenAIChat(api_key=API_KEY, conversation=init_conversation, model=model, max_tokens=max_tokens)
 
-    # Iniciamos la clase con el modelo, max_tokens, la API_KEY y la conversación inicial
-    openai_chat = OpenAIChat(api_key=API_KEY, conversation=init_conversation,  model=model, max_tokens=max_tokens)
-
-    # Pasamos al modelo la base de datos que hemos cargado de los ficheros
+    # Pass the loaded database content to the model
     response = openai_chat.get_response(db_content)
     print(response)
 
     if response == "1":
-
         while True:
-
             content = input(" ")
 
             if content == "exit":
@@ -59,11 +52,10 @@ def main():
                 break
 
             response = openai_chat.get_response(content)
-
             print(response)
             openai_chat.add_context_response(response)
     else:
-        print("La carga inicial de la base de datos no ha funcionado")
+        print("Initial loading of the database has failed")
 
 
 if __name__ == "__main__":
